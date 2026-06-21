e9f8ce1c3f5510ccf22a948de57162d21441d8ba
Preston Corless <pgattic@gmail.com>
2026-06-20 20:45:49 -0600

    Hashmap: airplane implementation
    Made on an airplane with no access to online documentation or external
    references

---
+++ src/hash_map.rs
@@ -0,0 +1,61 @@
+
+/// Custom Hash Map implementation
+pub struct HashMap<A> {
+    entries: [Vec<(String, A)>; 256],
+}
+
+impl<A: Clone> HashMap<A> {
+    pub fn new() -> Self {
+        Self {
+            entries: [const { Vec::new() }; 256],
+        }
+    }
+
+    pub fn insert(&mut self, key: String, value: A) {
+        let hashed = hash(&key);
+        // If pre-existing key, update in place
+        for (m_key, m_val) in &mut self.entries[hashed] {
+            if *m_key == key {
+                *m_val = value;
+                return;
+            }
+        }
+        // Else add new entry
+        self.entries[hashed].push((key, value))
+    }
+
+    pub fn get(&self, key: String) -> Option<A> {
+        let hashed = hash(&key);
+        for (m_key, m_val) in &self.entries[hashed] {
+            if *m_key == key {
+                return Some(m_val.clone());
+            }
+        }
+        None
+    }
+
+    pub fn remove(&mut self, key: String) -> Option<A> {
+        let hashed = hash(&key);
+        for (i, (m_key, m_val)) in (&self.entries[hashed].clone()).iter().enumerate() {
+            if *m_key == key {
+                let _ = &self.entries[hashed].remove(i);
+                return Some(m_val.clone());
+            }
+        }
+        None
+    }
+}
+
+fn hash(str: &str) -> usize {
+    sum_chars(str) % 256
+}
+
+fn sum_chars(str: &str) -> usize {
+    str.chars().map(key_code).sum()
+}
+
+fn key_code(ch: char) -> usize {
+    let ch_32: u32 = ch.into();
+    ch_32 as usize
+}
+
---
+++ tests/hash_map.rs
@@ -0,0 +1,37 @@
+
+mod hash_map_tests {
+    use algorithm_lib::HashMap;
+
+    #[test]
+    fn insert_and_retrieve() {
+        let mut my_map: HashMap<&str> = HashMap::new();
+        my_map.insert("Preston".to_string(), "Corless");
+        assert_eq!(my_map.get("Preston".to_string()), Some("Corless"));
+    }
+
+    #[test]
+    fn not_present() {
+        let mut my_map: HashMap<&str> = HashMap::new();
+        my_map.insert("Preston".to_string(), "Corless");
+        assert_eq!(my_map.get("notserP".to_string()), None);
+    }
+
+    #[test]
+    fn hash_collision() {
+        let mut my_map: HashMap<&str> = HashMap::new();
+        my_map.insert("Preston".to_string(), "Corless");
+        my_map.insert("notserP".to_string(), "sselroC");
+        assert_eq!(my_map.get("Preston".to_string()), Some("Corless"));
+        assert_eq!(my_map.get("notserP".to_string()), Some("sselroC"));
+        assert_eq!(my_map.get("rPeston".to_string()), None);
+    }
+
+    #[test]
+    fn insert_overwrite() {
+        let mut my_map: HashMap<&str> = HashMap::new();
+        my_map.insert("Skylar".to_string(), "Marriott");
+        my_map.insert("Skylar".to_string(), "Corless");
+        assert_eq!(my_map.get("Skylar".to_string()), Some("Corless"));
+    }
+}
+
