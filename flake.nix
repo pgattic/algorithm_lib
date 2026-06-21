e3893d55a736b8cb34ad2cba7d4d4d379cd7ad7e
Preston Corless <pgattic@gmail.com>
2026-06-20 20:46:05 -0600

    Flake: update inputs

--- flake.nix
+++ flake.nix
@@ -2,17 +2,21 @@
   description = "Algorithm-lib dev environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-    perSystem = { pkgs, self', ... }: {
+    perSystem = { pkgs, ... }: {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          cargo rustc rustfmt rust-analyzer
+        packages = [
+          pkgs.cargo
+          pkgs.clippy
+          pkgs.rust-analyzer
+          pkgs.rustc
+          pkgs.rustfmt
         ];
       };
     };
