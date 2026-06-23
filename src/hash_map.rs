c01a1baaa20e01f246dd7b570f132edf83d40252
Preston Corless <pgattic@gmail.com>
2026-06-23 06:58:33 -0600

    Hash Map: "entries" -> "buckets", use Hash trait

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -1,34 +1,35 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
 
 const NUM_BUCKETS: usize = 256;
 
 /// Custom Hash Map implementation
 pub struct HashMap<A> {
-    entries: [Vec<(String, A)>; NUM_BUCKETS],
+    buckets: [Vec<(String, A)>; NUM_BUCKETS],
 }
 
 impl<A: Clone> HashMap<A> {
     pub fn new() -> Self {
         Self {
-            entries: [const { Vec::new() }; NUM_BUCKETS],
+            buckets: [const { Vec::new() }; NUM_BUCKETS],
         }
     }
 
     pub fn insert(&mut self, key: String, value: A) {
         let hashed = hash(&key);
         // If pre-existing key, update in place
-        for (m_key, m_val) in &mut self.entries[hashed] {
+        for (m_key, m_val) in &mut self.buckets[hashed] {
             if *m_key == key {
                 *m_val = value;
                 return;
             }
         }
         // Else add new entry
-        self.entries[hashed].push((key, value))
+        self.buckets[hashed].push((key, value))
     }
 
     pub fn get(&self, key: String) -> Option<A> {
         let hashed = hash(&key);
-        for (m_key, m_val) in &self.entries[hashed] {
+        for (m_key, m_val) in &self.buckets[hashed] {
             if *m_key == key {
                 return Some(m_val.clone());
             }
@@ -38,7 +39,7 @@ impl<A: Clone> HashMap<A> {
 
     pub fn remove(&mut self, key: String) -> Option<A> {
         let hashed = hash(&key);
-        let bucket = &mut self.entries[hashed];
+        let bucket = &mut self.buckets[hashed];
         for (i, (m_key, _)) in bucket.iter().enumerate() {
             if *m_key == key {
                 return Some((*bucket).remove(i).1);
@@ -49,6 +50,8 @@ impl<A: Clone> HashMap<A> {
 }
 
 fn hash(str: &str) -> usize {
-    (str.bytes().fold(0usize, |a, b| a.wrapping_add(b as usize))) % NUM_BUCKETS
+    let mut hasher = DefaultHasher::new();
+    str.hash(&mut hasher);
+    (hasher.finish() % NUM_BUCKETS as u64) as usize
 }
 
