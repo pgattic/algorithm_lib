b46da08962544dc48d19b6d24f61207ecbd0add6
Preston Corless <pgattic@gmail.com>
2026-09-09 09:55:35 -0600

    Update inputs, nixpkgs -> channels.nixos.org

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Algorithm-lib dev environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
 
     perSystem = { pkgs, ... }: {
       devShells.default = pkgs.mkShell {
